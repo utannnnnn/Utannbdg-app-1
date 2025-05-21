9880dfbb87b02b4deca6d10ce8d1e47312ee0507
utannnnnn <sultan.syahatailahputra@gmail.com>
2025-05-21 09:21:42 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
 st.title("HELLO WELCOME TO BALLPOOL BOSS")
-st.write(
-    "Let's start GAME! For happy today 🥳🥳🥳."
-)
+st.write( "Let's start GAME! For happy today 🥳🥳🥳.")
+
+ 
