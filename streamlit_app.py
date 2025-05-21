a1120f32f21827ee861cbd9844e594d246eba662
utannnnnn <sultan.syahatailahputra@gmail.com>
2025-05-21 09:37:15 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title("HELLO WELCOME TO BALLPOOL BOSS")
 st.write( "Let's start GAME! For happy today 🥳🥳🥳.")
-st.image("https://github.com/utannnnnn/Utannbdg-app-1/blob/main/WhatsApp%20Image%202025-05-21%20at%2009.24.29_adea8059.jpg", width=200)
+st.image("WhatsApp%20Image%202025-05-21%20at%2009.24.29_adea8059.jpg", width=200)
  
